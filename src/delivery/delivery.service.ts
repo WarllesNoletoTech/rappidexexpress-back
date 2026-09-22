40a3d636da418496c0a8a5fecbcfa5e86962c6fa
WarllesNoletoTech <warllesb@gmail.com>
2026-09-22 01:42:25 -0300

    Usar colunas escalares indexadas nos filtros de entregas

--- src/delivery/delivery.service.ts
+++ src/delivery/delivery.service.ts
@@ -1074,14 +1074,14 @@ export class DeliveryService implements OnModuleInit {
     this.applyCityWhere(userForRequest, where, queryParams.cityId);
 
     if (userForRequest.type === UserType.MOTOBOY) {
-      where['motoboy.id'] = userForRequest.id;
+      where['motoboyId'] = userForRequest.id;
     }
 
     if (
       userForRequest.type === UserType.SHOPKEEPER ||
       userForRequest.type === UserType.SHOPKEEPERADMIN
     ) {
-      where['establishment.id'] = userForRequest.id;
+      where['establishmentId'] = userForRequest.id;
     }
 
     return where;
@@ -1105,13 +1105,13 @@ export class DeliveryService implements OnModuleInit {
     selectedCityId?: string,
   ) {
     if (userForRequest.type !== UserType.SUPERADMIN) {
-      where['establishment.cityId'] = userForRequest.cityId;
+      where['establishmentCityId'] = userForRequest.cityId;
       return;
     }
 
     const cityId = selectedCityId || userForRequest.cityId;
     if (cityId) {
-      where['establishment.cityId'] = cityId;
+      where['establishmentCityId'] = cityId;
     }
   }
 
@@ -1225,7 +1225,7 @@ export class DeliveryService implements OnModuleInit {
 
       if (normalizedMotoboyId) {
         const where = {};
-        where['motoboy.id'] = userFinded.id;
+        where['motoboyId'] = userFinded.id;
         where['isActive'] = true;
         where['status'] = {
           $in: [
@@ -1237,7 +1237,7 @@ export class DeliveryService implements OnModuleInit {
             StatusDelivery.AWAITING_CODE,
           ],
         };
-        where['establishment.cityId'] = userFinded.cityId;
+        where['establishmentCityId'] = userFinded.cityId;
 
         const deliveriesForMotoboy = await this.deliveryRepository.count(where);
 
@@ -2420,7 +2420,7 @@ export class DeliveryService implements OnModuleInit {
         id: deliveryId,
         isActive: true,
         status: StatusDelivery.ONCOURSE,
-        'motoboy.id': motoboyId,
+        motoboyId,
       } as any,
       {
         $set: {
@@ -2761,8 +2761,8 @@ export class DeliveryService implements OnModuleInit {
     ) {
       if (selectedStatuses.length) where['status'] = { $in: selectedStatuses };
       if (queryParams.establishmentId)
-        where['establishment.id'] = queryParams.establishmentId;
-      if (queryParams.motoboyId) where['motoboy.id'] = queryParams.motoboyId;
+        where['establishmentId'] = queryParams.establishmentId;
+      if (queryParams.motoboyId) where['motoboyId'] = queryParams.motoboyId;
       if (queryParams.createdBy) where['createdBy'] = queryParams.createdBy;
     }
 
@@ -2776,15 +2776,15 @@ export class DeliveryService implements OnModuleInit {
 
         // Se tiver um momento em que for necessario que o motoboy solicite todos os pedidos, ele vai conseguir ver tudo
         if (!selectedStatuses.includes(StatusDelivery.PENDING)) {
-          where['motoboy.id'] = userForRequest.id;
+          where['motoboyId'] = userForRequest.id;
         }
       } else {
-        where['motoboy.id'] = userForRequest.id;
+        where['motoboyId'] = userForRequest.id;
         where['status'] = { $ne: StatusDelivery.AWAITING_RELEASE };
       }
 
       if (queryParams.establishmentId)
-        where['establishment.id'] = queryParams.establishmentId;
+        where['establishmentId'] = queryParams.establishmentId;
     }
 
     //Lojistaadmin pode ver o mesmo que o lojista normal, unica diferença é que eles podem atribuir uma entrega ao motoboy
@@ -2792,9 +2792,9 @@ export class DeliveryService implements OnModuleInit {
       userForRequest.type === UserType.SHOPKEEPER ||
       userForRequest.type === UserType.SHOPKEEPERADMIN
     ) {
-      where['establishment.id'] = userForRequest.id;
+      where['establishmentId'] = userForRequest.id;
       if (selectedStatuses.length) where['status'] = { $in: selectedStatuses };
-      if (queryParams.motoboyId) where['motoboy.id'] = queryParams.motoboyId;
+      if (queryParams.motoboyId) where['motoboyId'] = queryParams.motoboyId;
     }
 
     // if (queryParams.hasOwnProperty('isActive')) {
